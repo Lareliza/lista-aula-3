81eb887475590c7b1ffa0b89acf05791b349385f
Lareliza <100451816+Lareliza@users.noreply.github.com>
2022-03-02 16:24:07 -0300

    Exercicio1

---
+++ lista-aula-3/src/main/java/Exercicio1.java
@@ -0,0 +1,14 @@
+public class Exercicio1 {
+
+    public static void main (String[] args){
+        int z1=130;
+        int z2=240;
+        int z3=320;
+        int total = z1+z2+z3;
+        int media = total/3;
+
+        System.out.println("Total:"+total);
+        System.out.println("Media:"+media);
+
+    }
+}
